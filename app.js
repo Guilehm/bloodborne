e64b74e48db00fde52a700db76f8e497af5213ce
guilherme <guile.hm@hotmail.com>
2019-07-14 15:28:56 -0300

    create app

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+
+const DEBUG = process.env.DEBUG
+const app = new express()
+
+app.use(morgan('short'))
+app.use(helmet())
+
+const getItemsController = require('./controllers/get-items-controller')
+
+app.get('/', getItemsController)
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+    let message = DEBUG ? 'Starting development server on port' : 'App listening on port'
+    console.log(message, `${PORT}`)
+})
