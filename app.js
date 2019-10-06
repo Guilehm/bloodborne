1cfd4a509f6529740fdbbb51657842e4ca0ab191
Guilherme Luis <guile.hm@hotmail.com>
2019-10-06 16:35:24 -0300

    setup mongodb, bodyparser, character create controller

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
-const express = require('express')
-const morgan = require('morgan')
-const helmet = require('helmet')
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const DEBUG = process.env.DEBUG
-const app = new express()
 
-app.use(morgan('short'))
-app.use(helmet())
+const DEBUG = process.env.DEBUG;
+const app = new express();
 
-const PORT = process.env.PORT || 4000
+
+const mongoUrl = process.env.MONGODB_URI;
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
+    .then(() => console.log('Connected to Mongo'))
+    .catch(e => console.log('Something went wrong', e));
+
+
+app.use(morgan('short'));
+app.use(helmet());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+const characterCreateController = require('./controllers/character-create-controller');
+
+app.post('/api/characters/', characterCreateController);
+
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    let message = DEBUG ? 'Starting development server on port' : 'App listening on port'
-    console.log(message, `${PORT}`)
-})
+    let message = DEBUG ? 'Starting development server on port' : 'App listening on port';
+    console.log(message, `${PORT}`);
+});
