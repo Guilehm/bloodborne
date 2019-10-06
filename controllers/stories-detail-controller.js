d56fc4876c69295797a0ed0064af79456939cf11
Guilherme Luis <guile.hm@hotmail.com>
2019-10-06 19:39:34 -0300

    create stories detail controller

---
+++ controllers/stories-detail-controller.js
@@ -0,0 +1,25 @@
+const Stories = require('../database/models/stories');
+
+module.exports = async (req, res) => {
+
+    let handleSuccess = data => {
+        if (!data) return handleNotFound();
+        return res.json(data);
+    };
+
+    let handleNotFound = () => {
+        let message = 'Stories not found.';
+        return res
+            .status(404)
+            .json({ message });
+    };
+
+    let handleError = data => {
+        return res.json(data);
+    };
+
+    let id = req.params.id;
+    await Stories.findOne({ id }, { _id: 0 })
+        .then(handleSuccess)
+        .catch(handleError);
+};
