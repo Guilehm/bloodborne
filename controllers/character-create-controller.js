3a6602d03e0f63ad1f61c7afefe73ef5997541ca
Guilherme Luis <guile.hm@hotmail.com>
2019-10-06 16:35:32 -0300

    create character create controller

---
+++ controllers/character-create-controller.js
@@ -0,0 +1,24 @@
+const Character = require('../database/models/character');
+
+module.exports = async (req, res) => {
+    let characterId = req.body.id;
+    let data = req.body;
+
+    let handleSuccess = rawData => {
+        let status = rawData.lastErrorObject.updatedExisting ? 200 : 201;
+        res.status(status).json(rawData.value);
+    };
+
+    let handleError = err => {
+        res.status(400).json(err);
+    };
+
+    await Character.findOneAndUpdate({ id: characterId }, data,
+        {
+            upsert: true,
+            new: true,
+            runValidators: true,
+            rawResult: true,
+        }
+    ).then(handleSuccess, handleError);
+};
