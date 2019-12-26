dd2d356f91288eef04624bf9777fe8dba1550646
guilherme <guile.hm@hotmail.com>
2019-12-26 00:56:23 -0300

    change to single quotes

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true })
 app.use(morgan('short'));
 app.use(helmet());
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 app.use(bodyParser.json());
