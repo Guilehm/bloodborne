3947607b8dad5035579edb37c6d87e2f31c71b89
guilherme <guile.hm@hotmail.com>
2020-05-18 00:47:42 -0300

    improve doLogin method

--- controllers/call-of-duty/api-configurator.js
+++ controllers/call-of-duty/api-configurator.js
@@ -7,10 +7,11 @@ const PASSWORD = process.env.COD_PASSWORD;
 
 class COD {
     constructor() {
-        this.baseURL = 'https://profile.callofduty.com/';
+        this.baseURL = 'https://profile.callofduty.com';
         this.username = USERNAME;
         this.password = PASSWORD;
-        this.headers = this.setHeaders();
+        this.instance = this.getAxiosInstance();
+        this.cookies = this.setCookies();
     }
 
     getAxiosInstance() {
@@ -20,14 +21,38 @@ class COD {
         });
     }
 
-    setHeaders() {
-        const setCookie = result => this.headers = ({
-            cookie: result.headers['set-cookie']
-        });
-        const instance = this.getAxiosInstance();
-        instance.get('cod/login')
-            .then(setCookie)
+    setCookies() {
+        const loginEndpoint = '/cod/login';
+        const saveCookie = result => this.cookies = result.headers['set-cookie'];
+        const instance = this.instance;
+        instance.get(loginEndpoint)
+            .then(saveCookie)
             .catch(err => console.log('deu merda', err));
+    }
+
+    doLogin() {
+        const authEndpoint = `${this.baseURL}/do_login?new_SiteId=cod`;
+        const XSRF = this.cookies
+            .find(c => c.includes('XSRF-TOKEN=')).split(';')[0].split('=')[1];
+        const headers = {
+            'Cookie': `XSRF-TOKEN=${XSRF}`
+        };
+        const data = {
+            'username': USERNAME,
+            'password': PASSWORD,
+            'remember_me': true,
+            '_csrf': `${XSRF}`,
+        };
+        const options = {
+            method: 'POST',
+            url: authEndpoint,
+            data,
+            headers,
+        };
+        const handleSuccess = result => result;
+        axios(options)
+            .then(handleSuccess)
+            .catch(err => console.log(err));
     }
 }
 
